3a51491653bd776cff9ed78d0e99a99012c64300
ftyszyx <whyzi@qq.com>
2017-07-27 10:23:21 +0800

    zyx

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
-var config=require("config/config.js")
+var config=require("./config/config.js")
 var app = express();
 
 
